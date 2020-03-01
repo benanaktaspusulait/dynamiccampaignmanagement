2eb612571694875d66bfcf5def24598824fa0d28
b.aktas <b.aktas@jrtechnologies.com>
2020-03-01 21:14:37 +0000

    offer creation change

--- src/main/scala/uk/hmrctest/dataseeder/OfferSeeder.scala
+++ src/main/scala/uk/hmrctest/dataseeder/OfferSeeder.scala
@@ -1,23 +1,23 @@
 package uk.hmrctest.dataseeder
 
-import uk.hmrctest.item.ItemType
+import uk.hmrctest.item.ItemType._
 import uk.hmrctest.offer.Offer
 
 object OfferSeeder {
 
   def createOffers: List[Offer] = {
 
-    val appleOffer = new Offer
-    appleOffer.name = "appleOffer"
-    appleOffer.description = "buy one, get one free on Apples: "
-    appleOffer.function = "function appleOffer(x) {return (Math.floor(x.appleCount() / 2)  * x.applePrice() )}"
-    appleOffer.itemType = ItemType.APPLE
+    var name = "appleOffer"
+    var description = "buy one, get one free on Apples: "
+    var function = "function appleOffer(x) {return (Math.floor(x.appleCount() / 2)  * x.applePrice() )}"
+    var itemType = APPLE
+    val appleOffer = Offer.apply(name, description, itemType,function)
 
-    val orangeOffer = new Offer
-    orangeOffer.name = "orangeOffer"
-    orangeOffer.description = "3 for the price of 2 on Oranges: "
-    orangeOffer.function = "function orangeOffer(x) {return (Math.floor(x.orangeCount() / 3)  * x.orangePrice() )}"
-    orangeOffer.itemType = ItemType.ORANGE
+    name = "orangeOffer"
+    description = "3 for the price of 2 on Oranges: "
+    function = "function orangeOffer(x) {return (Math.floor(x.orangeCount() / 3)  * x.orangePrice() )}"
+    itemType = ORANGE
+    val orangeOffer = Offer.apply(name, description, itemType,function)
 
     List(appleOffer,orangeOffer)
   }
--- src/main/scala/uk/hmrctest/offer/Offer.scala
+++ src/main/scala/uk/hmrctest/offer/Offer.scala
@@ -2,8 +2,12 @@ package uk.hmrctest.offer
 
 import uk.hmrctest.item.ItemType.ItemType
 
-class Offer(var name: String, var description: String, var itemType: ItemType,
+case class Offer(var name: String, var description: String, var itemType: ItemType,
             var function: String) {
 
   def this() = this(null, null, null, null)
+
+  def apply(name: String, description: String, itemType: ItemType, function: String): Offer =
+                                                             Offer(name, description, itemType, function)
+
 }
